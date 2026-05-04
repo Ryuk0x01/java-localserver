111149262634f79c6ee3323b4eb7d07b8ff26db2
Soufain Hachimi <hachimisoufain11@gmail.com>
2026-05-04 08:54:22 -0700

    setup logging

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("[DEBUG] main invoked");
         // Load the config first
         Config.loadConfig("config.json");
         
-}}
+        // Start the server with the config
+        Server server = new Server();
+        server.start();
+    }
+}
+ 
+ 
